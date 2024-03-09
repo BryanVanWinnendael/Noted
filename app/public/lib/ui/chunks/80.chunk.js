c1ee466e08a15a6169896abe93e3bd085ffe851b
Bryan Van Winnendael <71456367+BryanVanWinnendael@users.noreply.github.com>
2024-03-09 19:02:43 +0100

    Delete app/public/lib directory

--- app/public/lib/ui/chunks/80.chunk.js
+++
@@ -1,9 +0,0 @@
-;(window.webpackJsonp = window.webpackJsonp || []).push([
-  [80],
-  {
-    1739: function (n, e, t) {
-      t(43)({ target: "Number", stat: !0 }, { isInteger: t(533) })
-    },
-  },
-])
-//# sourceMappingURL=80.chunk.js.map
