f612113a6b9270c01bab9942f47c7ff3bb156613
BryanVanWinnendael <bryan.vanwinnendael@skynet.be>
2024-07-10 16:39:01 +0200

    removed react-editor-js

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -1,20 +1,27 @@
 import EditorWrapper from "@lib/styling/EditorWrapper";
 import { EDITOR_JS_TOOLS } from "@lib/tools";
-import { createReactEditorJS } from "react-editor-js";
+import EditorJS from '@editorjs/editorjs';
+import { useEffect } from "react";
 
 const Editor = ({ data, style }: any) => {
-  const ReactEditorJS = createReactEditorJS();
+
+  useEffect(() => {
+    const editor = new EditorJS({
+      holder: "noted",
+      data: JSON.parse(data),
+      tools: EDITOR_JS_TOOLS,
+    });
+
+    return () => {
+      editor.isReady.then(() => {
+        editor.destroy();
+      });
+    };
+  },[])
 
   return (
     <EditorWrapper style={JSON.parse(style)}>
-      <ReactEditorJS
-        holder={"noted"}
-        placeholder="Write your note here..."
-        defaultValue={JSON.parse(data)}
-        tools={EDITOR_JS_TOOLS}
-      >
-        <div id={"noted"}></div>
-      </ReactEditorJS>
+      <div id={"noted"}></div>
     </EditorWrapper>
   );
 };
